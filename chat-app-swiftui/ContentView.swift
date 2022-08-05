d7ecbca19569206c9166cc4dabed8d1aa3646eae
Shashank Mishra <shashankm@exzeo.com>
2022-08-05 22:58:36 +0530

    Initial Commit

---
+++ chat-app-swiftui/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  chat-app-swiftui
+//
+//  Created by Shashank Mishra on 05/08/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ chat-app-swiftui/chat_app_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  chat_app_swiftuiApp.swift
+//  chat-app-swiftui
+//
+//  Created by Shashank Mishra on 05/08/22.
+//
+
+import SwiftUI
+
+@main
+struct chat_app_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
